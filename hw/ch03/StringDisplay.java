994d8e8b3bea6c04c30c275bf1b2ccf05b23550f
nhaixb-bit <jjanjjanjjang@gmail.com>
2026-03-30 22:54:10 +0900

    3장 과제 코드 수정

--- hw/ch03/StringDisplay.java
+++ hw/ch03/StringDisplay.java
@@ -1,11 +1,18 @@
 package hw.ch03;
 
 public class StringDisplay extends AbstractDisplay {
-    private String string;  // 표시해야 하는 문자열 
+    private String string;  // 표시해야 하는 문자열
     private int width;      // 문자열의 길이
 
-    // 생성자 
+    // 기본 반복 횟수(5회)를 사용하는 생성자
     public StringDisplay(String string) {
+        this.string = string;
+        this.width = string.length();
+    }
+
+    // 반복 횟수를 직접 지정하는 생성자
+    public StringDisplay(String string, int repeatCount) {
+        super(repeatCount);
         this.string = string;
         this.width = string.length();
     }
@@ -25,7 +32,6 @@ public class StringDisplay extends AbstractDisplay {
         printLine();
     }
 
-    // open과 close에서 호출되어 "+----+" 문자열을 표시하는 메소드
     private void printLine() {
         System.out.print("+");
         for (int i = 0; i < width; i++) {
